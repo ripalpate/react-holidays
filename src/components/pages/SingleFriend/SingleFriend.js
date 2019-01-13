5e23317dbaa69ef7ff770d94fe8532be4ba51548
Ripal Patel <ripal212007@gmail.com>
2019-01-13 15:32:49 -0600

    delete button is displaying

--- src/components/pages/SingleFriend/SingleFriend.js
+++ src/components/pages/SingleFriend/SingleFriend.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './SingleFriend.scss';
 import friendsShape from '../../../helpers/propz/friendsShape';
-
+import authRequests from '../../../helpers/data/authRequests';
 class SingleFriend extends React.Component {
   static propTypes = {
     message: friendsShape,
@@ -9,6 +9,19 @@ class SingleFriend extends React.Component {
 
   render() {
     const { friend } = this.props;
+    const uid = authRequests.getCurrentUid();
+    const makeButtons = () => {
+      if (friend.uid === uid) {
+        return (
+            <span className="">
+              <button className="btn btn-danger">
+                <i className="fas fa-trash-alt"></i>
+              </button>
+            </span>
+        );
+      }
+    };
+
     return (
         <div className="card ml-4 single-friend-card bg-light mb-3">
           <h5 className="card-header">{friend.name}</h5>
@@ -16,6 +29,7 @@ class SingleFriend extends React.Component {
             <p className="card-text"> Address: {friend.address}</p>
             <p className="card-text">Phone Number: {friend.phoneNumber}</p>
             <p className="card-text">Email: {friend.email}</p>
+            <p className="card-text">{makeButtons()}</p>
           </div>
         </div>
     );
