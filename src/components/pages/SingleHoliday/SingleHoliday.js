aa60c1715e6a46914b051be1a78f647de08e520f
Ripal Patel <ripal212007@gmail.com>
2019-01-15 19:53:04 -0600

    style completed

--- src/components/pages/SingleHoliday/SingleHoliday.js
+++ src/components/pages/SingleHoliday/SingleHoliday.js
@@ -5,14 +5,17 @@ class SingleHoilday extends React.Component {
   render() {
     const { holiday } = this.props;
     return (
-    <div className="card ml-4 single-friend-card bg-light mb-3">
-          <h5 className="card-header">{holiday.name}</h5>
-          <div className="card-body">
-            <p className="card-text"> Date: {holiday.Date}</p>
-            <p className="card-text">Location: {holiday.location}</p>
-            <p className="card-text">Start Time: {holiday.startTime}</p>
-          </div>
+      <div className="card ml-4 single-holiday-card bg-light mb-3">
+        <div className="img-holder">
+        <img className="card-img-top holiday-img" src={holiday.imageUrl} alt="holiday"/>
         </div>
+        <div className="card-body">
+          <h5 className="card-title">Name: {holiday.name}</h5>
+          <p className="card-text"> Date: {holiday.Date}</p>
+          <p className="card-text">Location: {holiday.location}</p>
+          <p className="card-text">Start Time: {holiday.startTime}</p>
+        </div>
+      </div>
     );
   }
 }
