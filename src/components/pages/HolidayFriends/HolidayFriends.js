c83a4e526e9c972619c7a757ed47aaca2b5a829b
Ripal Patel <ripal212007@gmail.com>
2019-01-18 14:22:42 -0600

    WIP

--- src/components/pages/HolidayFriends/HolidayFriends.js
+++ src/components/pages/HolidayFriends/HolidayFriends.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import './HolidayFriends.scss';
+import friendsRequests from '../../../helpers/data/friendsRequests';
 
 class EditHoliday extends React.Component {
+  state = {
+    friends: [],
+  }
+
+  componentDidMount() {
+    friendsRequests.getFriendsRequest()
+      .then((friends) => {
+        this.setState({ friends });
+      }).catch(err => console.error(err));
+  }
+
   render() {
+    // const { friends } = this.state;
     return (
       <div className="EditHoliday">
         <h3>Add Friends to Holiday Page</h3>
+        <div></div>
       </div>
     );
   }
