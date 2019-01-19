aa3b206898907a3282b6b3d8e064c6a79cd3a86e
Ripal Patel <ripal212007@gmail.com>
2019-01-19 10:51:02 -0600

    checkbox is displaying with the holiday name

--- src/components/pages/HolidayFriends/HolidayFriends.js
+++ src/components/pages/HolidayFriends/HolidayFriends.js
@@ -2,26 +2,38 @@ import React from 'react';
 import './HolidayFriends.scss';
 import friendsRequests from '../../../helpers/data/friendsRequests';
 import authRequests from '../../../helpers/data/authRequests';
+import holidaysRequests from '../../../helpers/data/holidaysRequests';
 
 class HolidayFriends extends React.Component {
 
   state = {
+    singleHoliday: [],
     friends: [],
   }
 
   componentDidMount() {
     const uid = authRequests.getCurrentUid();
+    const firebaseId = this.props.match.params.id;
     friendsRequests.getFriendsRequest(uid)
       .then((friends) => {
         this.setState({ friends });
       }).catch(err => console.error(err));
+
+    holidaysRequests.getSingleHoliday(firebaseId)
+      .then((singleHoliday) => {
+        this.setState({ singleHoliday });
+      }).catch(err => console.error(err));
   }
 
   render() {
-    const { friends } = this.state;
+    const { friends, singleHoliday } = this.state;
     const friendnames = friends.map(friend => (
       <div key={friend.id} className="">
         <h5 className="card-header">{friend.name}</h5>
+        <div className="form-check">
+          <input type="checkbox" aria-label="Checkbox for following text input"/>
+          <label className="form-check-label" htmlFor="exampleCheck1">Attending {singleHoliday.name} </label>
+        </div>
       </div>));
     return (
       <div className="EditHoliday">
